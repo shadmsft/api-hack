16552780aeab9fd7c6fb18c74a0b9317fcbfb317
Shad Phillips <shadph@microsoft.com>
2022-07-18 12:14:37 -0500

    added workflow

--- api-web/Controllers/TokenController.cs
+++ api-web/Controllers/TokenController.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using api_web.Models;
+using Microsoft.Identity.Web;
 
 namespace api_web.Controllers
 {
     public class TokenController : Controller
     {
+        private readonly ITokenAcquisition _tokenAcquisition;
+        private readonly IConfiguration _configuration;
+
+        public TokenController(ITokenAcquisition tokenAcquisition, IConfiguration configuration)
+        {
+            _tokenAcquisition = tokenAcquisition;
+            _configuration = configuration;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<string> scopes = new List<string>(_configuration.GetSection("DownstreamApi:Scopes").Value.Split());
+            var accessToken = _tokenAcquisition.GetAccessTokenForUserAsync(scopes);
+
+            AuthInfo authInfo = new AuthInfo();
+            authInfo.accessToken = accessToken.Result.ToString();
+
+
+            return View(authInfo);
+
         }
     }
 }
---
+++ api-web/Models/AuthInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+using Microsoft.Identity.Web;
+
+namespace api_web.Models
+{
+    public class AuthInfo
+    {
+        public string accessToken { get; set; }
+    }
+}
